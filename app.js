adbc29030354f7408d981a7109c9fe9bd2662c7b
thaistavares <thais.tavares@gmail.com>
2022-07-18 11:54:32 -0300

    feat: conexão ao banco de dados OK

--- app.js
+++ app.js
@@ -2,11 +2,37 @@
 
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
+const mysql = require('mysql2');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', function(req,res){
-    res.send('Hello, World');
+const port = 3000;
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-app.listen(3000,function(){
-    console.log('Servidor em execução');
+db.connect((err)=>{
+    if(err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+app.set('port',port);
+app.set('views', __dirname.concat('/views'));
+app.set('view engine','ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(fileUpload());
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
