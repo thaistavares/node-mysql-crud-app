cb561c7b8dbff8f49873a311f3ffa2046e4c550d
thaistavares <thais.tavares@gmail.com>
2022-07-18 14:05:24 -0300

    feat: aplicação completa

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const {getHomePage} = require('./routes/index');
+const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
+
 const port = 3000;
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -33,6 +36,12 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(fileUpload());
+app.get('/',getHomePage);
+app.get('/add',addPlayerPage);
+app.get('/edit/:id', editPlayerPage);
+app.get('/delete/:id', deletePlayer);
+app.post('/add', addPlayer);
+app.post('/edit/:id', editPlayer);
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
---
+++ routes/player.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+module.exports = {
+    addPlayerPage: (req, res) => {
+        res.render('add-player.ejs', {
+            title: 'Welcome to Socka | Add a new player',
+            message: ''
+        });
+    },
+    addPlayer: (req, res) => {
+        if (!req.files) {
+            return res.status(400).send("No files were uploaded.")
+        }
+        let message = '';
+        const first_name = req.body.first_name;
+        const last_name = req.body.last_name;
+        const position = req.body.position;
+        const number = req.body.number;
+        const username = req.body.username;
+        const uploadedFile = req.filmes.image;
+        const fileExtension = uploadedFile.mimetype.split('/')[1];
+        const image_name = username + '.' + fileExtension;
+        const usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + username + "'";
+
+        db.query(usernameQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            if (result.length > 0) {
+                message = 'Username already exists';
+                res.render('add-player.ejs', {
+                    message: message,
+                    title: 'Welcome to Socka | Add a new player'
+                });
+            } else {
+                if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
+                    uploadedFile.mv(`public/assets/img/${image_name}`, (err) => {
+                        if (err) {
+                            return res.status(500).send(err);
+                        }
+
+                        const query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name) VALUES ('" +
+                            first_name + "', '" + last_name + "','" + position + "','" + number + "','" + image_name + "','" + username + "')";
+                        db.query(query, (err, result) => {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+                            res.redirect('/');
+                        });
+                    });
+                } else {
+                    message = "Invalid file format. Only 'gif', 'jpg' and 'png' formats are allowed.";
+                    res.render('add-player.ejs', {
+                        message: message,
+                        title: 'Welcome to Socka | Add a new player'
+                    });
+                }
+            }
+        });
+    },
+    editPlayerPage: (req, res) => {
+        const playerId = req.params.id;
+        const query = "SELECT * FROM `players` WHERE id='" + playerId + "';";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.render('edit-player.ejs', {
+                title: 'Edit player',
+                player: result[0],
+                message: ''
+            });
+        });
+    },
+    editPlayer: (req, res) => {
+        const playerId = req.params.id;
+        const first_name = req.body.first_name;
+        const last_name = req.body.last_name;
+        const position = req.body.position;
+        const number = req.body.number;
+        const query = "UPDATE `players` SET `first_name` = '" + first_name + "',`last_name` = '" + last_name + "',`position` = '" + position + "',`number` = '" + number + "' WHERE `players`.`id` = '" + playerId + "';";
+        db.query(query, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.redirect('/');
+        });
+    },
+    deletePlayer: (req, res) => {
+        const playerId = req.params.id;
+        const getImageQuery = "SELECT image from `players` WHERE id = '" + playerId + "';";
+        const deleteUserQuery = "DELETE from `players` WHERE id = '" + playerId + "';";
+        db.query(getImageQuery, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            const image = result[0].image;
+            fs.unlink(`public/assets/img/${image}`,(err) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                db.query(deleteUserQuery, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                res.redirect('/');
+                });
+            });
+        });
+    }
+};
